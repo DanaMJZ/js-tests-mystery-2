da637b4d37ca72916dda52a1dd53bb1c493d04c4
Dana Al Shaar <da.alshaar@gmail.com>
2025-05-21 11:11:13 -0700

    mystery 2 tests passed

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,6 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  if ((num % 400 === 0) || (num % 4 === 0 && num % 100 !== 0)) {
+    return true;
+  }
+  return false;
+};
